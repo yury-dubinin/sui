972db6893532d94ce72ec169bdc0583a9d6e7122
laura <laura@mystenlabs.com>
2023-12-18 18:39:48 +0900

    add back feature gating to profiler as struct attribute

--- crates/sui-types/src/gas_model/tables.rs
+++ crates/sui-types/src/gas_model/tables.rs
@@ -9,6 +9,7 @@ use move_core_types::gas_algebra::{AbstractMemorySize, InternalGas, NumArgs, Num
 use move_core_types::language_storage::ModuleId;
 
 use move_core_types::vm_status::StatusCode;
+#[cfg(feature = "gas-profiler")]
 use move_vm_profiler::GasProfiler;
 use move_vm_types::gas::{GasMeter, SimpleInstruction};
 use move_vm_types::loaded_data::runtime_types::Type;
@@ -75,7 +76,7 @@ pub struct GasStatus {
     instructions_executed: u64,
     instructions_next_tier_start: Option<u64>,
     instructions_current_tier_mult: u64,
-
+    #[cfg(feature = "gas-profiler")]
     profiler: Option<GasProfiler>,
 }
 
@@ -113,6 +114,7 @@ impl GasStatus {
             stack_height_next_tier_start,
             stack_size_next_tier_start,
             instructions_next_tier_start,
+            #[cfg(feature = "gas-profiler")]
             profiler: None,
         }
     }
@@ -140,6 +142,7 @@ impl GasStatus {
             stack_height_next_tier_start: None,
             stack_size_next_tier_start: None,
             instructions_next_tier_start: None,
+            #[cfg(feature = "gas-profiler")]
             profiler: None,
         }
     }
@@ -661,10 +664,12 @@ impl GasMeter for GasStatus {
         self.gas_left
     }
 
+    #[cfg(feature = "gas-profiler")]
     fn get_profiler_mut(&mut self) -> Option<&mut GasProfiler> {
         self.profiler.as_mut()
     }
 
+    #[cfg(feature = "gas-profiler")]
     fn set_profiler(&mut self, profiler: GasProfiler) {
         self.profiler = Some(profiler);
     }
