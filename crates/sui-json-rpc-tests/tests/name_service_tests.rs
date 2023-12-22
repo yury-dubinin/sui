e781578c5a3a43d78477a7e7c5be17e911c1bbd0
Manolis Liolios <manos@mystenlabs.com>
2023-12-22 13:32:22 +0200

    oops, fixing formatting again

--- crates/sui-json-rpc-tests/tests/name_service_tests.rs
+++ crates/sui-json-rpc-tests/tests/name_service_tests.rs
@@ -7,68 +7,61 @@ use sui_json_rpc::name_service;
 #[test]
 fn test_name_service_outputs() {
     assert_eq!(
-        name_service::Domain::from_str("@test").unwrap().to_string() == "test.sui",
-        true
+        name_service::Domain::from_str("@test").unwrap().to_string(),
+        "test.sui"
     );
     assert_eq!(
         name_service::Domain::from_str("test.sui")
             .unwrap()
-            .to_string()
-            == "test.sui",
-        true
+            .to_string(),
+        "test.sui"
     );
     assert_eq!(
         name_service::Domain::from_str("@test@sui")
             .unwrap()
-            .to_string()
-            == "test.sui",
-        true
+            .to_string(),
+        "test.sui"
     );
     assert_eq!(
         name_service::Domain::from_str("test@test@sui")
             .unwrap()
-            .to_string()
-            == "test.test.sui",
-        true
+            .to_string(),
+        "test.test.sui"
     );
     assert_eq!(
         name_service::Domain::from_str("test.test")
             .unwrap()
-            .to_string()
-            == "test.test.sui",
-        true
+            .to_string(),
+        "test.test.sui"
     );
     assert_eq!(
         name_service::Domain::from_str("test@test")
             .unwrap()
-            .to_string()
-            == "test.test.sui",
-        true
+            .to_string(),
+        "test.test.sui"
     );
     assert_eq!(
         name_service::Domain::from_str("test@sui")
             .unwrap()
-            .to_string()
-            == "test.sui",
-        true
+            .to_string(),
+        "test.sui"
     );
     assert_eq!(
-        name_service::Domain::from_str("test").unwrap().to_string() == "test.sui",
-        true
+        name_service::Domain::from_str("test").unwrap().to_string(),
+        "test.sui"
     );
 }
 
 #[test]
 fn test_tld_formatted_sld_outputs() {
     assert_eq!(
-        name_service::Domain::from_str("@sui").unwrap().to_string() == "sui.sui",
-        true
+        name_service::Domain::from_str("@sui").unwrap().to_string(),
+        "sui.sui"
     );
     assert_eq!(
         name_service::Domain::from_str("@sui@sui")
             .unwrap()
-            .to_string()
-            == "sui.sui",
-        true
+            .to_string(),
+        "sui.sui"
     );
 }
